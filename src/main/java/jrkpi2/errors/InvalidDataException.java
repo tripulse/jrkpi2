b573c6109b6d4ef3d9f3f6e5b403bd6e1f4508b6
tripulse <bsantanu381@gmail.com>
2020-09-27 16:39:50 +0530

    Begone useless constructor overload

--- src/main/java/jrkpi2/errors/InvalidDataException.java
+++ src/main/java/jrkpi2/errors/InvalidDataException.java
@@ -4,10 +4,6 @@ package jrkpi2.errors;
  * {@code InvalidDataException} denotes data of some sort was not like as desired to be.
  */
 public class InvalidDataException extends RuntimeException {
-    public InvalidDataException() {
-        super();
-    }
-
     public InvalidDataException(String message) {
         super(message);
     }
